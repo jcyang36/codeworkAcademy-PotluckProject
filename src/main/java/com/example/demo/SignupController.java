d091f65f8045a520550f5e62ca6eb9bbea2eabad
Joyce Yang <jcyang@hmc.edu>
2017-06-21 12:01:43 -0400

    Going to fix database

--- src/main/java/com/example/demo/SignupController.java
+++ src/main/java/com/example/demo/SignupController.java
@@ -3,10 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by student on 6/21/17.
@@ -20,6 +17,17 @@ public class SignupController {
         model.addAttribute("signup", new Signup());
         return "signup";
     }
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewSignup(@RequestParam String name,@RequestParam String food)
+    {Signup s = new Signup();
+    s.setName(name);
+    s.setFood(food);
+    signupRepository.save(s);
+    return "Saved";
+    }
+
+
     @PostMapping("/signup")
     public String signupSubmit(@ModelAttribute Signup signup){
         return "result";
